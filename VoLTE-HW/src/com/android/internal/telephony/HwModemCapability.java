f24c087dc9c96b2787b785eadafbf6b58818de9e
penn5 <penn5@users.noreply.github.com>
2018-10-01 13:00:04 +0100

    Update HwModemCapability.java (#3)
    Reimplement the stock one, so that if it is supported, we get the feature!

--- VoLTE-HW/src/com/android/internal/telephony/HwModemCapability.java
+++ VoLTE-HW/src/com/android/internal/telephony/HwModemCapability.java
@@ -1,11 +1,39 @@
 package com.android.internal.telephony;
 
 public class HwModemCapability {
-	public static boolean isCapabilitySupport(int cap) {
-		//Support adjust speech codec
-		if(cap == 11) {
+	public static boolean isCapabilitySupport(int capability) {
+		boolean z = true;
+		int bcdIndex = capability / 4;
+		int bcdOffset = capability % 4;
+		if (capability < 0 || capability >= 360) {
 			return false;
+		}
+		if (TextUtils.isEmpty(MODEM_CAP)) {
+			MODEM_CAP = SystemProperties.get("persist.radio.modem.cap", LogException.NO_VALUE);
+		}
+		try {
+			int bcdValue = convertChar2Int(MODEM_CAP.charAt(bcdIndex));
+			if (bcdValue != -1) {
+				if (((1 << (3 - bcdOffset)) & bcdValue) <= 0) {
+					z = false;
+				}
+				return z;
+			}
+		} catch (IndexOutOfBoundsException ex) {
+			Log.e(TAG, "isCapabilitySupport " + ex);
 		}
 		return false;
 	}
+	private int convertChar2Int(char origChar) {
+		if (origChar >= '0' && origChar <= '9') {
+			return origChar - 48;
+		}
+		if (origChar >= 'a' && origChar <= 'f') {
+			return (origChar - 97) + 10;
+		}
+		if (origChar < 'A' || origChar > 'F') {
+			return -1;
+		}
+		return (origChar - 65) + 10;
+        }
 };

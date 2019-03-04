e83e5d7436af59cc2996f99359d35bd895f6f75c
Black-Seraph <admin@black-seraph.com>
2019-03-04 21:21:16 +0000

    Fix VoLTE-CAF build-script to work with Xiaomi Mi Max 3

--- VoLTE-CAF/build.sh
+++ VoLTE-CAF/build.sh
@@ -38,13 +38,19 @@ cp "$system_folder"/lib64/libunwind.so "$libdst"
 cp "$system_folder"/lib64/libhardware.so "$libdst"
 cp "$system_folder"/lib64/libhwbinder.so "$libdst"
 cp "$system_folder"/lib64/liblzma.so "$libdst"
+cp "$system_folder"/lib64/libbufferhubqueue.so "$libdst"
+cp "$system_folder"/lib64/libpdx_default_transport.so "$libdst"
+cp "$system_folder"/lib64/libutilscallstack.so "$libdst"
+cp "$system_folder"/lib64/libcrypto.so "$libdst"
+cp "$system_folder"/lib64/libunwindstack.so "$libdst"
+cp "$system_folder"/lib64/libdexfile.so "$libdst"
 
-for i in android.hardware.media@1.0.so android.hardware.graphics.common@1.0.so android.hidl.token@1.0.so android.hardware.graphics.mapper@2.0.so android.hardware.graphics.allocator@2.0.so android.hidl.token@1.0-utils.so android.hardware.graphics.bufferqueue@1.0.so android.hardware.configstore@1.0.so android.hardware.configstore-utils.so;do
+for i in android.hardware.media@1.0.so android.hardware.graphics.common@1.0.so android.hardware.graphics.common@1.1.so android.hidl.token@1.0.so android.hardware.graphics.mapper@2.0.so android.hardware.graphics.mapper@2.1.so android.hardware.graphics.allocator@2.0.so android.hidl.token@1.0-utils.so android.hardware.graphics.bufferqueue@1.0.so android.hardware.configstore@1.0.so android.hardware.configstore@1.1.so android.hardware.configstore-utils.so;do
 	newName="$(echo "$i" |sed -E -e 's/^andr/libA/g' -e 's/@/-/g')"
 	cp "$system_folder"/lib64/$i "$libdst"/$newName
 done
 
-for i in android.hardware.media@1.0.so android.hardware.graphics.common@1.0.so android.hidl.token@1.0.so android.hardware.graphics.mapper@2.0.so android.hardware.graphics.allocator@2.0.so android.hidl.token@1.0-utils.so android.hardware.graphics.bufferqueue@1.0.so android.hardware.configstore@1.0.so android.hardware.configstore-utils.so;do
+for i in android.hardware.media@1.0.so android.hardware.graphics.common@1.0.so android.hardware.graphics.common@1.1.so android.hidl.token@1.0.so android.hardware.graphics.mapper@2.0.so android.hardware.graphics.mapper@2.1.so android.hardware.graphics.allocator@2.0.so android.hidl.token@1.0-utils.so android.hardware.graphics.bufferqueue@1.0.so android.hardware.configstore@1.0.so android.hardware.configstore@1.1.so android.hardware.configstore-utils.so;do
 	newName="$(echo "$i" |sed -E -e 's/^andr/libA/g' -e 's/@/-/g')"
 	sed -i -E "s/$i/$newName/g" "$libdst"/*.so
 done
